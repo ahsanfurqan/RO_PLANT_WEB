394a10711a6bc6d80312d7c06badc95f0f66b4ff
Huzaifakhan110 <huzaifakaimkhani1@gmail.com>
2021-09-08 07:04:45 -0700

    first commit

---
+++ include/footer.php
@@ -0,0 +1,26 @@
+<div class="lime-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <span class="footer-text">2019 © stacks</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+       <!-- Javascripts -->
+       <script src="../assets/plugins/jquery/jquery-3.1.0.min.js"></script>
+        <script src="../assets/plugins/bootstrap/popper.min.js"></script>
+        <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+        <script src="../assets/js/lime.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" 
+        integrity="sha256-yE5LLp5HSQ/z+hJeCqkz9hdjNkk1jaiGG0tDCraumnA=" 
+        crossorigin="anonymous"
+></script>
+        <script>$("#Total").mask('0000000');</script>
+       
+    </body>
+</html>
