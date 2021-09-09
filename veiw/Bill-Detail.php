a017fba9e9b3f7cd552bc825e025d3f4d7d31980
ahsanfurqan <ahsanfurqan.bukc@gmail.com>
2021-09-09 22:59:02 +0500

    bill_details

--- veiw/Bill-Detail.php
+++ veiw/Bill-Detail.php
@@ -7,7 +7,7 @@
                             <div class="page-title">
                                 
                                 <h3>Bill Details</h3>
-                                    <form>
+                                    <form id="bill_details">>
                                         <div class="form-row">
                                             <div class="form-group col-md-6">
                                                 <label for="inputname">Client ID</label>
@@ -30,8 +30,8 @@
         
       </tr>
     </thead>
-    <tbody>
-           
+    <tbody id='table_data'>
+<!--            
       <tr class="">
         <td></td>
         <td></td>
@@ -67,7 +67,7 @@
         <td></td>
        
       </tr>
-      
+       -->
     </tbody>
   </table>
                                     </form>
@@ -76,3 +76,38 @@
                         </div>
                     </div>
 <?php include '..\include\footer.php'?> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+   $('#bill_details').submit(function(event){
+                            event.preventDefault();
+                            var name=$('#inputname').val();
+                            
+                            $.ajax({
+                                url:"http://192.168.0.183:8000/api/search/bill/"+name,
+                                // data:formdata,
+                                type:'GET',
+                                success: function(result){
+                                  // alert(result[0].bill_id);
+                                    if(result[0].bill_id){
+                                      for(var i=0;i<result.length; i++){
+                                      var html='<tr>';
+                                      html+='<td>'+result[i].bill_id+'</td>';
+                                      html+='<td>'+result[i].client_id+'</td>';
+                                      html+='<td>'+result[i].used_bottles+'</td>';
+                                      
+                                      html+='<td>'+result[i].amount+'</td></tr>';
+                                    //   html.SetEditable();
+                                    // alert(result[i].name);
+                                      $('#table_data').append(html);
+                                      }
+                                    //   $('#num1').SetEditable();
+                                      
+                                      // $('#com_details')[0].reset();
+                                    }
+                                },
+                                error:function(result){
+                                    console.log(result)
+                                }
+                            });
+                        })
+  </script>
