0e9cb6064a878be4cc375431cf7cf35c95565ec1
mohammadzain007 <mohammadzain132@gmail.com>
2021-09-21 22:06:16 +0500

    portal

---
+++ RO_Plant_Web/veiw/portal.php
@@ -0,0 +1,90 @@
+<?php include '../include/header.php'?>
+<!-- <div class="lime-container">
+            
+                <div class="container">
+                <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Free</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+      </div> -->
+                    <!-- <div class="row">
+                    <div class="col-xl">
+                            <div class="card" style="width:70%; display:content;">
+                                <div class="card-body" >
+                                    
+                                <div class="row">
+                                <style type="text/css">
+	
+	</style>
+	<div style="margin-top: 4%"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="col-md-11">
+					<div class="well candidate">
+						<h1 class="text-center">Company</h1>
+						<a id="BodyPH_hlCandidate" class="btn btn-login" href="">login</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="col-md-11">
+					<div class="well student">
+						<h1 class="text-center">Customer</h1>
+						<a id="BodyPH_hlStudent" class="btn btn-login" href="">login</a>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+</div> -->
+<!-- </div>
+</div>
+</div> -->
+<div class="lime-container">
+            
+                <div class="container">
+<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm" >
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Company</h4>
+          </div>
+          <div class="card-body" style="height:100px;">
+           
+            <button type="button" class="w-50 btn btn-lg btn-warning">Register</button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm" >
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Customer</h4>
+          </div>
+          <div class="card-body" style="height:100px;">
+           
+            <button type="button" class="w-50 btn btn-lg btn-primary" >Login</button>
+          </div>
+        </div>
+      </div>
+      
+          
+        </div>
+      </div>
+    </div>
+</div>
+</div>  
+                                <?php include '../include/footer.php'?>
+                            
